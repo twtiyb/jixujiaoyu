3c9e0da14f761957f97cafdcc8973b201ac16871
徐纯 <chunchun.xu@wwwarehouse.com>
2017-08-10 23:40:41 +0800

    add rule

--- polls/rule/models.py
+++ polls/rule/models.py
@@ -20,35 +20,64 @@ class RuleRelation(db.Entity):
     rule = Required(Rule)
     s_person = Required(Person, reverse="s_ruleRelations")
     t_person = Required(Person, reverse="t_ruleRelations")
+    root = Optional('RuleRelation', reverse='root')
+
+#
+# class RootNode(db.Entity):
+#     id = PrimaryKey(int, auto=True)
+#     rule = Required(Rule)
+#     s_person = Required(Person, reverse="s_ruleRelations")
+#     t_person = Required(Person, reverse="t_ruleRelations")
 
 
 db.generate_mapping(create_tables=True)
 sql_debug(True)
 
-if __name__ == '__main__':
-    with db_session:
-        # exam1 = Exam(name='test',testFinish=False)
-        #
-        # q1 = Question(outId='abcdssss1231',name='测试题目1',exam = exam1)
-        # q2 = Question(outId='abcdssss1232',name='测试题目2',exam = exam1)
-        #
-        # qa1 = Answer(value='正确',question=q1)
-        # qa2 = Answer(value='错误',question=q1)
-        #
-        # e1 = select(p for p in Exam).first()
+@db_session
+def test_basicData():
+    # e1 = select(p for p in Exam).first()
+    # cleanRelation()
+    rule = Rule(name='拣选')
 
-        rule = Rule(name='拣选')
+    zhongshuo = Person(name='钟硕')
+    huangjiang = Person(name='黄江')
+    wangze = Person(name='王泽')
+    taozhen = Person(name='陶臻')
+    xuchun = Person(name='徐纯')
 
-        zhongshuo = Person(name='钟硕')
-        huangjiang = Person(name='黄江')
-        wangze = Person(name='王泽')
-        taozhen = Person(name='陶臻')
-        xuchun = Person(name='徐纯')
+    # 简单的授权
+    rootZ = RuleRelation(rule=rule, s_person=zhongshuo, t_person=zhongshuo)
+    # rootZ.root = rootZ
+    z2w = RuleRelation(rule=rule, s_person=zhongshuo, t_person=wangze)
+    w2t = RuleRelation(rule=rule, s_person=wangze, t_person=taozhen)
+    t2x = RuleRelation(rule=rule, s_person=taozhen, t_person=xuchun)
 
-        z2w = RuleRelation(rule=rule, s_person=zhongshuo, t_person=wangze)
-        w2t = RuleRelation(rule=rule, s_person=wangze, t_person=taozhen)
-        t2x = RuleRelation(rule=rule, s_person=taozhen, t_person=xuchun)
+    commit()
 
 
+@db_session
+def cleanData():
+    db.execute('delete from RuleRelation')
+    db.execute('delete from Rule')
+    db.execute('delete from Person')
+    commit()
 
-        commit()
+@db_session
+def cleanRelation():
+    db.execute('delete from RuleRelation')
+    commit()
+
+
+@db_session
+def test_basic():
+    commit()
+
+
+@db_session
+def test_basic2():
+    commit()
+
+
+# 是否存在相同父节点
+def existsNode(person, lineNode):
+    return True;
---
+++ polls/rule/tree.py
@@ -0,0 +1,107 @@
+class node:
+    def __init__(self, data):
+        self._data = data
+        self._children = []
+
+    def getdata(self):
+        return self._data
+
+    def getchildren(self):
+        return self._children
+
+    def add(self, node):
+        ##if full
+        if len(self._children) == 4:
+            return False
+        else:
+            self._children.append(node)
+
+    def go(self, data):
+        for child in self._children:
+            if child.getdata() == data:
+                return child
+        return None
+
+
+class tree:
+    def __init__(self):
+        self._head = node('header')
+
+    def linktohead(self, node):
+        self._head.add(node)
+
+    def insert(self, path, data):
+        cur = self._head
+        for step in path:
+            if cur.go(step) == None:
+                return False
+            else:
+                cur = cur.go(step)
+        cur.add(node(data))
+        return True
+
+    def search(self, path):
+        cur = self._head
+        for step in path:
+            if cur.go(step) == None:
+                return None
+            else:
+                cur = cur.go(step)
+        return cur
+
+
+'''
+define node
+'''
+a = node('A')
+b = node('B')
+c = node('C')
+d = node('D')
+e = node('E')
+f = node('F')
+g = node('G')
+h = node('H')
+i = node('I')
+j = node('J')
+k = node('K')
+l = node('L')
+m = node('M')
+n = node('N')
+o = node('O')
+
+'''
+adding node to build true
+'''
+a.add(b)
+a.add(g)
+a.add(h)
+b.add(c)
+b.add(e)
+g.add(i)
+g.add(j)
+g.add(k)
+g.add(l)
+h.add(m)
+h.add(n)
+h.add(o)
+c.add(d)
+c.add(f)
+i.add(node(29))
+j.add(node(28))
+k.add(node(27))
+l.add(node(26))
+m.add(node(25))
+n.add(node(24))
+o.add(node(23))
+f.add(node(30))
+
+tree = tree()
+tree.linktohead(a)
+
+# testcase
+print('Node', tree.search("ABE").getdata())
+print('Node', tree.search("ABC").getdata())
+print('Node', tree.search("AHM").getdata())
+tree.insert("ABCD", 1)
+for i in d.getchildren():
+    print('value after', d.getdata(), ' is ', i.getdata())
